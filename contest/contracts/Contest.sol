36ca0835f8e82ca711d7f1309bba99503b4743dd
justinmnl1093 <justinmnl@hotmail.com>
2018-11-04 19:29:06 +0530

    Two contestants are assigned

--- contest/contracts/Contest.sol
+++ contest/contracts/Contest.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.4.24;
 //creating the contract
 contract Contest {
 	
-	//declaring the variables
-	string public contestant;
-
-	//declaring constructor
-	function Contest() public {
-		contestant = "Tom";
+	//creating structure to model the contestant
+	struct Contestant {
+		uint id;
+		string name;
+		uint voteCount;
 	}
-}
+
+	//use mapping to get contestant details
+	mapping (uint => Contestant) public contestant;
+
+	//add a public state variable to keep track of contestant count
+	uint public contestantCount;
+
+	//add constructor
+	constructor() public {
+		addContestant("Tom");
+		addContestant("Jerry");
+	}
+	
+	//add a function to add contestants
+	function addContestant (string _name) private {
+		contestantCount ++;
+		contestant[contestantCount] = Contestant(contestantCount, _name, 0);	
+	}	
+
+} 
